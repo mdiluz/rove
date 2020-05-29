b5b41c95a5b2401fff4425fa44ec1cd408b861f6
Marc Di Luzio <marc.diluzio@gmail.com>
2020-05-29 18:13:26 +0100

    Add SIGTERM handler and logging
    	Also change port to default http

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 
+	fmt.Println("Initialising...")
+
+	// Set up the close handler
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("SIGTERM recieved, exiting...")
+		os.Exit(0)
+	}()
+
 	// Create a new router
 	router := NewRouter()
 
+	fmt.Println("Initialised")
+
 	// Listen and serve the http requests
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	fmt.Println("Serving HTTP")
+	if err := http.ListenAndServe(":80", router); err != nil {
 		log.Fatal(err)
 	}
 }
