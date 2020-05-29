5377e42e710bd128cbe67377b4c4a03352294a94
Marc Di Luzio <marc.diluzio@gmail.com>
2020-05-29 17:41:11 +0100

    Add a basic server router to main

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", HandleRoot)
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
