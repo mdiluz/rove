a6d86c258103ba622ebdf4fa5292b18f94a4f24b
Marc Di Luzio <marc.diluzio@gmail.com>
2020-05-29 23:05:16 +0100

    Add an integration test

--- cmd/rove-server/integration_test.go
+++ cmd/rove-server/integration_test.go
@@ -1,10 +1,19 @@
 // +build integration
+
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/mdiluz/rove/pkg/rove"
+)
 
 var serverUrl = "http://localhost:8080"
 
 func TestServerStatus(t *testing.T) {
-
+	conn := rove.NewConnection(serverUrl)
+	status := conn.Status()
+	if !status.Ready {
+		t.Error("Server did not return that it was ready")
+	}
 }
