5f358fa9fdda288bce7d7ba69f9ed88bedfb4a35
Marc Di Luzio <marc.diluzio@gmail.com>
2020-05-30 22:53:12 +0100

    Fix the integration test and the default port for the compose

--- cmd/rove-server/integration_test.go
+++ cmd/rove-server/integration_test.go
@@ -8,12 +8,13 @@ import (
 	"github.com/mdiluz/rove/pkg/rove"
 )
 
-var serverUrl = "http://localhost:8080"
+var serverUrl = "localhost:8080"
 
 func TestServerStatus(t *testing.T) {
 	conn := rove.NewConnection(serverUrl)
-	status := conn.Status()
-	if !status.Ready {
+	if status, err := conn.Status(); err != nil {
+		t.Errorf("Status returned error: %s", err)
+	} else if !status.Ready {
 		t.Error("Server did not return that it was ready")
 	}
 }
