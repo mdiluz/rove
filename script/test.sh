95963e9b29be169d95537fda05acc0ced8fd23c7
Marc Di Luzio <marc.diluzio@gmail.com>
2020-05-31 11:40:07 +0100

    Add test script

---
+++ script/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+cd ..
+set -x
+
+# Test the build
+go build -v ./...
+
+# Run unit tests
+go test -v ./... -cover
+
+# Verify docker build
+docker build .
+
+# Run the integration tests with docker-compose
+docker-compose up --build --detach
+go test -v ./... -tags integration -cover
+docker-compose down
