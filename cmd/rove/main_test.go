c0d4a809c9f88d61a84ad1b23a207adbafd101a6
Marc Di Luzio <marc.diluzio@gmail.com>
2020-07-26 17:31:09 +0100

    Update command line client to allow specifying command number

--- cmd/rove/main_test.go
+++ cmd/rove/main_test.go
@@ -54,8 +54,12 @@ func Test_InnerMain(t *testing.T) {
 	assert.NoError(t, InnerMain("command", "stash"))
 	assert.NoError(t, InnerMain("command", "repair"))
 	assert.NoError(t, InnerMain("command", "broadcast", "abc"))
+	assert.NoError(t, InnerMain("command", "10", "wait"))
+	assert.NoError(t, InnerMain("command", "1", "wait", "turn", "NW", "toggle", "broadcast", "zyx"))
 
 	// Give it malformed commands
 	assert.Error(t, InnerMain("command", "unknown"))
 	assert.Error(t, InnerMain("command", "broadcast"))
+	assert.Error(t, InnerMain("command", "0", "wait"))
+	assert.Error(t, InnerMain("command", "1"))
 }
